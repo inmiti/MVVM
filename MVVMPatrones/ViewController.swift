24a4eb96de24ecb55fcbc715a2b2575c987a28a0
inmiti <inmabauvel@gmail.com>
2023-10-03 20:33:23 +0200

    Initial Commit

---
+++ MVVMPatrones/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMPatrones
+//
+//  Created by ibautista on 3/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
